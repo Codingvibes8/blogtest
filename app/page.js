a6195d4968eadb9a4baa5868cd7286d6a415d772
Codingvibes8 <wecodeappz@gmail.com>
2024-10-27 01:07:48 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>blogposts</h1>
+      <h1 className={'text-green-400 text-3xl'}>blogposts</h1>
+        <p className={'text-slate-200 text-3xl'}>this is the first blog update</p>
      
     </div>
   );
